0e1e63003d23472c334cb7ddc83c9c6825f506f8
George Makoveckij <Itemfouund@gmail.com>
2020-05-13 21:07:02 +0300

    Added chairs, chairs settings panel and something other

---
+++ Assets/Scripts/Chair.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class Chair : MonoBehaviour
+{
+    private GameObject _chairSettingsPanel;
+
+    public void AddNewPlayer()
+    {
+        const int numPosition = 5;
+        var chairNum = Convert.ToInt32(gameObject.name[numPosition]);
+        
+        
+        
+    }
+}
